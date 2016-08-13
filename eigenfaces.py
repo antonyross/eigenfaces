9a8f4a7e13ed2fd6b40d65ea65ebe342da9ad66e
Povilas Balciunas <balciunas90@gmail.com>
2016-08-13 15:55:31 +0300

    Move read_images out of EigenFaces class
    read_images() does not use any class instance variables like self.variable.
    This implies that this method might be a standalone function.
    Having it as a separate function simplifies EigenFaces class, allows more flexible use, etc.

--- eigenfaces.py
+++ eigenfaces.py
@@ -10,55 +10,6 @@ import pca
 
 
 class EigenFaces(object):
-    def read_images(self, path, sz=None):
-        """Reads the images in a given folder, resizes images on the fly if size is given.
-
-        Args:
-            path: Path to a folder with subfolders representing the subjects (persons).
-            sz: A tuple with the size Resizes
-
-        Returns:
-            A tuple of (images, image_labels, class_matrix_list) where
-                images: The images, which is a Python list of numpy arrays.
-                image_labels: The corresponding labels (the unique number of
-                the subject, person).
-        """
-        class_samples_list = []
-        class_matrices_list = []
-        images, image_labels = [], []
-        for dirname, dirnames, filenames in os.walk(path):
-            for subdirname in dirnames:
-                subject_path = os.path.join(dirname, subdirname)
-                class_samples_list = []
-                for filename in os.listdir(subject_path):
-                    if filename != ".DS_Store":
-                        try:
-                            im = Image.open(os.path.join(subject_path, filename))
-                            # resize to given size (if given) e.g., sz = (480, 640)
-                            if (sz is not None):
-                                im = im.resize(sz, Image.ANTIALIAS)
-                            images.append(np.asarray(im, dtype = np.uint8))
-
-                        except IOError as e:
-                            errno, strerror = e.args
-                            print("I/O error({0}): {1}".format(errno, strerror))
-                        except:
-                            print("Unexpected error:", sys.exc_info()[0])
-                            raise
-                        # adds each sample within a class to this List
-                        class_samples_list.append(np.asarray(im, dtype = np.uint8))
-
-                # flattens each sample within a class and adds the array/vector to a class matrix
-                class_samples_matrix = np.array([img.flatten()
-                    for img in class_samples_list],'f')
-
-                 # adds each class matrix to this MASTER List
-                class_matrices_list.append(class_samples_matrix)
-
-                image_labels.append(subdirname)
-
-        return images, image_labels, class_matrices_list
-
     def train(self, root_training_images_folder):
         self.list_of_arrays_of_images = []
         self.labels_list = []
@@ -69,7 +20,7 @@ class EigenFaces(object):
 
         self.list_of_arrays_of_images, self.labels_list, \
             list_of_matrices_of_flattened_class_samples = \
-                self.read_images(root_training_images_folder)
+                read_images(root_training_images_folder)
 
          # create matrix to store all flattened images
         images_matrix = np.array([np.array(Image.fromarray(img)).flatten()
@@ -154,3 +105,53 @@ def show_image_for(img_class):
     predicted_img = "training_images/%s/1.pgm" % (img_class)
     img = Image.open(predicted_img)
     img.show()
+
+
+def read_images(path, sz=None):
+    """Reads the images in a given folder, resizes images on the fly if size is given.
+
+    Args:
+        path: Path to a folder with subfolders representing the subjects (persons).
+        sz: A tuple with the size Resizes
+
+    Returns:
+        A tuple of (images, image_labels, class_matrix_list) where
+            images: The images, which is a Python list of numpy arrays.
+            image_labels: The corresponding labels (the unique number of
+            the subject, person).
+    """
+    class_samples_list = []
+    class_matrices_list = []
+    images, image_labels = [], []
+    for dirname, dirnames, filenames in os.walk(path):
+        for subdirname in dirnames:
+            subject_path = os.path.join(dirname, subdirname)
+            class_samples_list = []
+            for filename in os.listdir(subject_path):
+                if filename != ".DS_Store":
+                    try:
+                        im = Image.open(os.path.join(subject_path, filename))
+                        # resize to given size (if given) e.g., sz = (480, 640)
+                        if (sz is not None):
+                            im = im.resize(sz, Image.ANTIALIAS)
+                        images.append(np.asarray(im, dtype = np.uint8))
+
+                    except IOError as e:
+                        errno, strerror = e.args
+                        print("I/O error({0}): {1}".format(errno, strerror))
+                    except:
+                        print("Unexpected error:", sys.exc_info()[0])
+                        raise
+                    # adds each sample within a class to this List
+                    class_samples_list.append(np.asarray(im, dtype = np.uint8))
+
+            # flattens each sample within a class and adds the array/vector to a class matrix
+            class_samples_matrix = np.array([img.flatten()
+                for img in class_samples_list],'f')
+
+             # adds each class matrix to this MASTER List
+            class_matrices_list.append(class_samples_matrix)
+
+            image_labels.append(subdirname)
+
+    return images, image_labels, class_matrices_list
